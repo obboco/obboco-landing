1150bba79da8cb454dcd1b8c8da7eb5543983c35
Albert Playa <albertplayavidal@gmail.com>
2022-07-18 08:54:42 +0200

    Nuxt config improvements

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,14 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
     exposeConfig: true
+  },
+  head: {
+    title: 'nuxt-tailwindcss',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   }
 });
