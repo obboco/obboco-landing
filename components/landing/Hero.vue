5073fe5a993b67af00660ea6894de2aab4ef49b8
Albert Playa <albertplayavidal@gmail.com>
2022-07-18 21:28:00 +0200

    Title hero gradient

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -2,7 +2,7 @@
   <div class="hero min-h-screen">
     <div class="hero-content text-center">
       <div class="max-w-4xl">
-        <h1 class="text-7xl font-bold">
+        <h1 class="title text-7xl font-bold py-2">
           The issue tracking tool you'll enjoy using
         </h1>
         <p class="text-3xl py-6">
@@ -14,3 +14,11 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.title {
+  background: -webkit-linear-gradient(#48ac98, #2a6879);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
