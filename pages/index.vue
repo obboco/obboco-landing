3225f88ff6c86b7535d7005c40f8ae9439d5b584
Albert Playà Vidal <albertplayavidal@gmail.com>
2023-03-04 15:43:18 +0100

    Improve meta tags

--- pages/index.vue
+++ pages/index.vue
@@ -15,10 +15,19 @@ export default defineNuxtComponent({
           hid: 'Obboco',
           name: 'description',
           content:
-            'Open source booking system: Obboco.com helps you to manage your activities, bookings, customers and revenue with no effort. Free for individuals and Open Source'
+            'Obboco.com is a open source booking system. Helps you to manage your activities, bookings, customers and revenue with no effort. Free and open Source'
         },
-        { hid: 'og:type', name: 'og:type', content: 'website' },
-        { hid: 'og:url', name: 'og:url', content: 'https:obboco.com' },
+        {
+          hid: 'itemprop-name',
+          itemprop: 'name',
+          content: 'Obboco.com | Open Source booking system'
+        },
+        {
+          hid: 'itemprop-description',
+          itemprop: 'description',
+          content:
+            'Obboco.com is a open source booking system. Helps you to manage your activities, bookings, customers and revenue with no effort. Free and open Source'
+        },
         {
           hid: 'og:title',
           name: 'og:title',
@@ -30,7 +39,13 @@ export default defineNuxtComponent({
           content:
             'Open source booking system: Obboco.com helps you to manage your activities, bookings, customers and revenue with no effort. Free for individuals and open source'
         },
-        { hid: 'og:url', name: 'og:url', content: 'https:obboco.com' }
+        { hid: 'og:type', name: 'og:type', content: 'website' },
+        { hid: 'og:url', name: 'og:url', content: 'https:obboco.com' },
+        {
+          hid: 'og:site_name',
+          name: 'og:site_name',
+          content: 'Obboco.com | Open Source booking system'
+        }
       ],
       link: [{ rel: 'icon', type: 'image/svg', href: '/favicon.ico' }]
     };
